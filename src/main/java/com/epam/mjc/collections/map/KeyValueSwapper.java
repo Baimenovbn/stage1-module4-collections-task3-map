07b7bbf5bc2ba691d8b541e6b83b23f4aa33bfdf
Baimenovbn <baibn@mail.ru>
2023-01-02 12:46:15 +0600

    feat: finish task

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,27 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        HashMap<String, Integer> swapped = new HashMap<>();
+
+        for (Map.Entry<Integer, String> entry: sourceMap.entrySet()) {
+            String shouldBeKey = entry.getValue();
+            int shouldBeValue = entry.getKey();
+
+            if (swapped.containsKey(shouldBeKey)) {
+                int swappedValue = swapped.get(shouldBeKey);
+
+                if (shouldBeValue < swappedValue) {
+                    swapped.put(entry.getValue(), shouldBeValue);
+                }
+            } else {
+                swapped.put(shouldBeKey, shouldBeValue);
+            }
+        }
+
+        return swapped;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,25 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] words = sentence.split(" ");
+        HashMap<String, Integer> wordRepetitionQuantity = new HashMap<>();
+
+        if (!sentence.isEmpty()) {
+            for (String word: words) {
+                String formatted = word.trim().replaceAll("[.,:-]", "").toLowerCase();
+
+                if (wordRepetitionQuantity.containsKey(formatted)) {
+                    wordRepetitionQuantity.put(formatted, wordRepetitionQuantity.get(formatted) + 1);
+                } else {
+                    wordRepetitionQuantity.put(formatted, 1);
+                }
+            }
+        }
+
+        return wordRepetitionQuantity;
     }
 }
